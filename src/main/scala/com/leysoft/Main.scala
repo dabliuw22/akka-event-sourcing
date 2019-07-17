a65bcbfd29b442e9c0b58633d6185bde04b2796d
Will Leyton <wleylop22@gmail.com>
2019-07-17 11:18:23 -0500

    Add persistAll logic

--- src/main/scala/com/leysoft/Main.scala
+++ src/main/scala/com/leysoft/Main.scala
@@ -1,16 +1,18 @@
 package com.leysoft
 
 import akka.actor.ActorSystem
-
 import com.leysoft.local.{LocalSalesActor, Product}
-
-import java.util.Date
+import java.util.{Date, UUID}
 
 object Main extends App {
   val system = ActorSystem("event-sourcing-system")
 
-  val salesActor = system.actorOf(LocalSalesActor.props("id-local"),
-    "sales-actor")
+  val salesActor = system.actorOf(LocalSalesActor
+    .props(UUID.randomUUID().toString), "sales-actor")
+  var products: List[Product] = Nil
 
   for(i <- 1 to 10) salesActor ! Product(i.toString, i * 100, new Date())
+  for(i <- 11 to 20) products =
+    products.appended(Product(i.toString, i * 100, new Date()))
+  salesActor ! products
 }
--- src/main/scala/com/leysoft/local/LocalSalesActor.scala
+++ src/main/scala/com/leysoft/local/LocalSalesActor.scala
@@ -7,26 +7,61 @@ import akka.persistence.PersistentActor
 
 class LocalSalesActor(val id: String) extends PersistentActor with ActorLogging {
 
-  var generatedId: Long = 0L
+  var count: Long = 0L
 
-  var totalAmount: Long = 0L
+  var total: Long = 0L
 
-  override def persistenceId: String = id
-
-  override def receiveCommand: Receive = {
-    case product: Product =>
-      persist(Sale(generatedId, product)) {event =>
-        generatedId += 1
-        totalAmount += product.amount
-        log.info(s"Event: $event, total: $totalAmount")
-      }
+  val f = (event: Sale) => {
+    count += 1
+    total += event.product.amount
+    log.info(s"Event: $event, count: $count, total: $total")
   }
 
+  /**
+    * Identificador único.
+    */
+  override def persistenceId: String = id
+
+  /**
+    * Default receive. En este método recibimos los mensajes y persistimos
+    * eventos, además de realizar un manejo de los eventos y cambio de estado
+    * por parte del actor de forma segura.
+    */
+  override def receiveCommand: Receive = {
+    case product: Product =>
+      persist(Sale(count, product)) {f}
+    case products: List[Product] =>
+      persistAll(products.zip(count to (count + products.length))
+        .map(pair => Sale(pair._2, pair._1))) {f}
+  }
+
+  /**
+    * En este método se recuperan los eventos persistidos luego de un inicio
+    * o reinicio del actor y se realiza la actualización del estado del actor.
+    */
   override def receiveRecover: Receive = {
     case sale: Sale =>
-      generatedId = sale.id
-      totalAmount += sale.product.amount
-      log.info(s"Recover: ${sale.id}, total: $totalAmount")
+      count = sale.id
+      total += sale.product.amount
+      log.info(s"Recover: ${sale}, count: $count, total: $total")
+  }
+
+  /**
+    * Método llamado si persist() falla. Detendra (STOP) al actor, por lo
+    * que se recomeinda iniciar al actor dentro de un tiempo.
+    */
+  override def onPersistFailure(cause: Throwable, event: Any, seqNr: Long): Unit = {
+    log.error(s"Fail to persist event: $event because $cause")
+    super.onPersistFailure(cause, event, seqNr)
+  }
+
+  /**
+    * Método llamado si el Journal falla al persistir el evento. El actor
+    * pasara a RESUME.
+    */
+  override def onPersistRejected(cause: Throwable, event: Any, seqNr: Long): Unit = {
+    log.error(s"Persist rejected for event: $event because $cause")
+    super.onPersistRejected(cause, event, seqNr)
   }
 }
 
